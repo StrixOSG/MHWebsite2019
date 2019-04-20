0a76aad544c116f2f9566416d7b66a87335d7efc
StrixOSG <6631874+StrixOSG@users.noreply.github.com>
2019-04-19 21:49:19 -0600

    Initial setup. Webpack, Sass, NodeJS, ReactJS, Babel, and some optimizers and other plugins

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './sass/bootstrap/scss/bootstrap.scss';
+import "./sass/main.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './pages/home';
+
+ReactDOM.render(
+    
+)

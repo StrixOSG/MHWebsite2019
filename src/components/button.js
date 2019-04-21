3ec459652a1a2f331a3c65b765cf685b244e09b7
StrixOSG <6631874+StrixOSG@users.noreply.github.com>
2019-04-20 22:53:00 -0600

    removed code from main.scss as it wasn't necessary anymore

--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 
-
 export default class Header extends React.Component{
 
     render(){
